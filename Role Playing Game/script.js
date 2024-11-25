2bf8702bf0d116aa73c85fd85cfda0af727385ce
me.learn <provinesmilan@gmail.com>
2024-11-25 23:30:22 +0545

    Add monster array plus add sell weapon feature

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -24,6 +24,24 @@ const weapons = [
     { name: 'claw hammer', power: 50 },
     { name: 'sword', power: 100 }
 ];
+const monsters = [
+    {
+        name: "slime",
+        level: 2,
+        health: 15
+    },
+    {
+        name: "fanged beast",
+        level: 8,
+        health: 60
+    },
+    {
+        name: "dragon",
+        level: 20,
+        health: 300
+    }
+];
+
 const locations = [
     {
         name: "town square",
@@ -91,10 +109,6 @@ function goCave() {
     update(locations[2]);
 }
 
-function fightDragon() {
-    console.log("Fighting dragon.");
-}
-
 function buyHealth() {
     if (gold >= 10) {
         gold -= 10;
@@ -121,14 +135,33 @@ function buyWeapon() {
         }
     } else {
         text.innerText = "You already have the most powerful weapon!";
+        button2.innerText = "Sell weapon for 15 gold";
+        button2.onclick = sellWeapon;
+    }
+}
+function sellWeapon() {
+    if (inventory.length > 1) {
+        gold += 15;
+        goldText.innerText = gold;
+        let currentWeapon = inventory.shift();
+        text.innerText = "You sold a " + currentWeapon + ".";
+        text.innerText += " In your inventory you have: " + inventory;
+    } else {
+        text.innerText = "Don't sell your only weapon!";
     }
 }
 
-
 function fightSlime() {
-
+    fighting = 0;
+    goFight();
 }
 
 function fightBeast() {
+
+}
+function fightDragon() {
+    console.log("Fighting dragon.");
+}
+function goFight() {
 
 }
