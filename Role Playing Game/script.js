4f0f9165b9b4f2cbae5b7167327565719c04d3ca
me.learn <provinesmilan@gmail.com>
2024-11-26 12:57:33 +0545

    Add another object kill monster to call locations[4]

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -66,6 +66,12 @@ const locations = [
         "button text": ["Attack", "Dodge", "Run"],
         "button functions": [attack, dodge, goTown],
         text: "You are fighting a monster."
+    },
+    {
+        name: "kill monster",
+        "button text": ["Go to town square", "Go to town square", "Go to town square"],
+        "button functions": [goTown, goTown, goTown],
+        text: "The monster screams Arg! as it dies. You gain experience points and find gold."
     }
 ];
 
@@ -200,4 +206,7 @@ function lose() {
 function defeatMonster() {
     gold += Math.floor(monsters[fighting].level * 6.7);
     xp += monsters[fighting].level;
+    goldText.innerText = gold;
+    xpText.innerText = xp;
+    update(locations[4]);
 }
