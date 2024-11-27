b2367d1c4d93a8c9f0c2e2ed9e27eb5d93cbb27c
me.learn <provinesmilan@gmail.com>
2024-11-27 19:07:00 +0545

    Make a function to check valid input-step38

--- Calorie Counter/script.js
+++ Calorie Counter/script.js
@@ -8,8 +8,21 @@ let isError = false;
 
 
 function cleanInputString(str) {
-    console.log("original string: ", str);
-    const regex = /[+-\s]/g;
-    return str.replace(regex, '');
+    const regex = /[+-\s]/g;// regex for pattern matching, g "global" means it will look even after it work is done
+    return str.replace(regex, '');//replace method to replace one string with another, it takes two parameters
 }
-console.log(cleanInputString("+-99"));
+
+function isInvalidInput(str) {
+    const regex = /\d+e\d+/i; //i makes HELLo, heElLo, hEllO, etc to CASE INSENSITIVE, \d for digits(0-9), e for characters
+    return str.match(regex);
+    //console.log(isInvalidInput("1e3")); OUTPUT: [ '1e3', index: 0, input: '1e3', groups: undefined ]
+    /**
+     * "1e3" is the matched value against the /\d+e\d+/i regex.
+    index: 0 is the index of the matched value in the string.
+    input: '1e3' is the original string that was matched.
+    groups: undefined are the matched groups, which are not used in this case 
+
+    console.log(isInvalidInput("10")); OUTPUT: null means no match is found
+
+    */
+}
