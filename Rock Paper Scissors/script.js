5c56cc935253e79d5f58c28f197844c6fdfcb197
me.learn <provinesmilan@gmail.com>
2024-11-30 22:33:37 +0545

    Update the showresult function to check if computer or player reached over 3 points and display winner message-step5

--- Rock Paper Scissors/script.js
+++ Rock Paper Scissors/script.js
@@ -94,10 +94,55 @@ const playerScoreSpanElement = document.getElementById("player-score");
 const computerScoreSpanElement = document.getElementById("computer-score");
 const roundResultsMsg = document.getElementById("results-msg");
 
+
+//STEP 5
+/*
+If you try to play the game, you will see that you can play for an infinite amount of rounds.
+ But the rules state that the first one to three points wins.
+
+Inside your showResults function, you will need to check if the player or computer 
+has reached three points. If either has reached three points, you should display a message indicating the winner.
+
+For example, if the player has won the game, then the winnerMsgElement should be 
+updated to "Player has won the game!". If the computer has won the game, then the winnerMsgElement should be 
+updated to "Computer has won the game!".
+
+If there is a winner, you will want to show the resetGameBtn button and hide the optionsContainer 
+so the player can play again.
+
+TIPS:
+Use the style.display property on an element, with the value "block" or "none", to show or hide the element.
+*/
+
+
 function showResults(userOption) {
-    playerScoreSpanElement.innerText = computerScore;
-    computerScoreSpanElement.innerText = playerScore;
     roundResultsMsg.innerText = getRoundResults(userOption);
+    computerScoreSpanElement.innerText = computerScore;
+    playerScoreSpanElement.innerText = playerScore;
+    if (playerScore >= 3 && computerScore <= 3) {
+        optionsContainer.style.display = "none";
+        resetGameBtn.style.display = "block";
+        return winnerMsgElement.innerText = "Player has won the game!";
+    } else {
+        optionsContainer.style.display = "none";
+        resetGameBtn.style.display = "block";
+        return winnerMsgElement.innerText = "Computer has won the game!";
+    }
+
 };
 
-showResults("Rock");
+const rockBtn = document.getElementById("rock-btn");
+const paperBtn = document.getElementById("paper-btn");
+const scissorsBtn = document.getElementById("scissors-btn");
+
+rockBtn.addEventListener("click", function () {
+    showResults("Rock");
+});
+
+paperBtn.addEventListener("click", function () {
+    showResults("Paper");
+});
+
+scissorsBtn.addEventListener("click", function () {
+    showResults("Scissors");
+});
