4c7169126995e434e78f90e956a4508c2a58fa49
me.learn <provinesmilan@gmail.com>
2024-11-30 21:55:49 +0545

    Create a function to get round results-step3

--- Rock Paper Scissors/script.js
+++ Rock Paper Scissors/script.js
@@ -2,11 +2,12 @@
 /**
  * The first step is to build out the function that will generate a random choice for the computer.
 
-The getRandomComputerResult function will be used to get the computer's choice. Inside that function, you should see an options array with "Rock", "Paper", and "Scissors".
-
+The getRandomComputerResult function will be used to get the computer's choice. 
+Inside that function, you should see an options array with "Rock", "Paper", and "Scissors".
 Your task is to complete the getRandomComputerResult function so that it returns a random option from the options array.
 TIPS:
-You can use Math.random() and Math.floor() to help you get a random whole number. This will represent the index number for the options array.
+You can use Math.random() and Math.floor() to help you get a random whole number. 
+This will represent the index number for the options array.
 You can use the random index to access the option from the options array.
  */
 
@@ -22,10 +23,11 @@ console.log(getRandomComputerResult());
 
 //STEP 2
 /**
- * Complete the hasPlayerWonTheRound function. This function has two parameters: player and computer. The function should return true if the player has won the round, and false if the player has lost or tied the round.
-
+ * Complete the hasPlayerWonTheRound function. This function has two parameters: player and computer. 
+ * The function should return true if the player has won the round, and false if the player has lost or tied the round.
+ * 
+TIPS:
 Here are the criteria for the player to win a round:
-
 If the player chooses "Rock" and the computer chooses "Scissors"
 If the player chooses "Scissors" and the computer chooses "Paper"
 If the player chooses "Paper" and the computer chooses "Rock"
@@ -41,3 +43,37 @@ function hasPlayerWonTheRound(player, computer) {
 
 console.log(hasPlayerWonTheRound("Rock", "Scissors"));
 console.log(hasPlayerWonTheRound("Scissors", "Rock"));
+
+
+//STEP 3
+/**
+ * Now it is time to get the results of the round. Complete the getRoundResults function.
+
+If the player wins the round, update the playerScore by 1 and return the message "Player wins! [player's choice] beats [computer's choice]".
+
+If the computer and player choose the same option, return the message "It's a tie! Both chose [player's choice]".
+
+If the computer wins the round, update the computerScore by 1 and return the message "Computer wins! [computer's choice] beats [player's choice]".
+
+[computer's choice] should be replaced with computerResult while [player's choice] should be replaced with the userOption.
+
+TIPS:
+Remember you can use the hasPlayerWonTheRound function to check if the player wins the round.
+You can use template literals or regular string concatenation to build the message.
+ */
+let playerScore = 0;
+let computerScore = 0;
+
+function getRoundResults(userOption) {
+    const computerResult = getRandomComputerResult();
+
+    if (hasPlayerWonTheRound(userOption, computerResult)) {
+        playerScore++;
+        return `Player wins! ${userOption} beats ${computerResult}`;
+    } else if (computerResult === userOption) {
+        return `It's a tie! Both chose ${userOption}`;
+    } else {
+        computerScore++;
+        return `Computer wins! ${computerResult} beats ${userOption}`;
+    }
+}
