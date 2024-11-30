1cbecf1586f8c7a1a86a8c36d9e85ce336e51fdd
me.learn <provinesmilan@gmail.com>
2024-11-30 21:34:16 +0545

    Add a function to check if player has won the round with certain criteria-step2

--- Rock Paper Scissors/script.js
+++ Rock Paper Scissors/script.js
@@ -17,3 +17,27 @@ function getRandomComputerResult() {
     return options[randomIndex];
 }
 console.log(getRandomComputerResult());
+
+
+
+//STEP 2
+/**
+ * Complete the hasPlayerWonTheRound function. This function has two parameters: player and computer. The function should return true if the player has won the round, and false if the player has lost or tied the round.
+
+Here are the criteria for the player to win a round:
+
+If the player chooses "Rock" and the computer chooses "Scissors"
+If the player chooses "Scissors" and the computer chooses "Paper"
+If the player chooses "Paper" and the computer chooses "Rock"
+ */
+
+function hasPlayerWonTheRound(player, computer) {
+    if (player === "Rock" && computer === "Scissors" || player === "Scissors" && computer === "Paper" || player === "Paper" && computer === "Rock") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(hasPlayerWonTheRound("Rock", "Scissors"));
+console.log(hasPlayerWonTheRound("Scissors", "Rock"));
