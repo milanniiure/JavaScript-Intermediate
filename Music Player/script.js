aaa9027917dbb70be59da73445e25ad599794452
me.learn <provinesmilan@gmail.com>
2024-12-02 23:03:37 +0545

    Create a function called setPlayerDisplay and setPlayButtonAccessibleText

--- Music Player/script.js
+++ Music Player/script.js
@@ -130,6 +130,9 @@ const playSong = (id) => {
     userData.currentSong = song;
     playButton.classList.add("playing");
 
+    highlightCurrentSong();
+    setPlayerDisplay();
+    setPlayButtonAccessibleText();
     audio.play();
 };
 
@@ -160,6 +163,18 @@ const playPreviousSong = () => {
         playSong(previousSong.id);
     }
 };
+
+const setPlayerDisplay = () => {
+    const playingSong = document.getElementById("player-song-title");
+    const songArtist = document.getElementById("player-song-artist");
+    const currentTitle = userData?.currentSong?.title;
+    const currentArtist = userData?.currentSong?.artist;
+
+    playingSong.textContent = currentTitle ? currentTitle : "";
+    songArtist.textContent = currentArtist ? currentArtist : "";
+};
+
+
 
 const highlightCurrentSong = () => {
     const playlistSongElements = document.querySelectorAll(".playlist-song");
@@ -212,6 +227,16 @@ const renderSongs = (array) => {
         }).join("");
     playlistSongs.innerHTML = songsHTML;
 };
+
+const setPlayButtonAccessibleText = () => {
+    const song = userData?.currentSong || userData?.songs[0];
+
+    playButton.setAttribute(
+        "aria-label",
+        song?.title ? `Play ${song.title}` : "Play"
+    );
+};
+
 const getCurrentSongIndex = () => userData?.songs.indexOf(userData?.currentSong);
 
 playButton.addEventListener("click", () => {
