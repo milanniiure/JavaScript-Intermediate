9af57496d009a07d132661c6f5a771cbe2be8b36
me.learn <provinesmilan@gmail.com>
2024-12-04 18:40:41 +0545

    Add arrow function with map and inside that call destructring assignment and return divs

--- Football Team Cards/script.js
+++ Football Team Cards/script.js
@@ -193,3 +193,22 @@ typeOfSport.textContent = sport;
 teamName.textContent = team;
 worldCupYear.textContent = year;
 headCoach.textContent = coachName;
+
+typeOfSport.textContent = sport;
+teamName.textContent = team;
+worldCupYear.textContent = year;
+headCoach.textContent = coachName;
+
+const setPlayerCards = (arr = players) => {
+
+
+    playerCards.innerHTML += arr.map(
+        ({ name, position, number, isCaptain, nickname }) => {
+            return `
+        <div class="player-card">
+        <h2>${name}</h2>
+        </div>
+        `;
+        }
+    );
+};
