34d48ea10ac9c46bc18f2e932585c81389d5c3c0
me.learn <provinesmilan@gmail.com>
2024-12-06 22:35:58 +0545

    Refactor the code of submit button which lets user edit  and delete tasks, move that into new function

--- Todo App/script.js
+++ Todo App/script.js
@@ -31,6 +31,22 @@ const addOrUpdateTask = () => {
     }
 };
 
+const updateTaskContainer = () => {
+    taskData.forEach(
+        ({ id, title, date, description }) => {
+            tasksContainer.innerHTML += `
+            <div class="task" id="${id}">
+            <p><strong>Title:</strong> ${title}</p>
+            <p><strong>Date:</strong> ${date}</p>
+            <p><strong>Description:</strong> ${description}</p>
+            <button type="button" class="btn">Edit</button>
+            <button type="button" class="btn">Delete</button>
+            </div>
+        `
+        }
+    );
+}
+
 const reset = () => {
     titleInput.value = "";
     dateInput.value = "";
