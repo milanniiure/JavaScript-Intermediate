520a9f746a37cc182a6629795d3a69d0f248bbcf
me.learn <provinesmilan@gmail.com>
2024-12-10 22:07:57 +0545

    Learn about decimal and binary conversion technique or method

--- Decimal to Binary Converter/script.js
+++ Decimal to Binary Converter/script.js
@@ -2,6 +2,13 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
+/*
+For the decimal to binary conversion, you need to divide input by 2 until the quotient, 
+or the result of dividing two numbers, is 0. But since
+ you don't know how many times you need to divide input by 2, you can use a 
+ while loop to run a block of code as long as input is greater than 0 and can be divided.
+*/
+
 
 const checkUserInput = () => {
     if (
@@ -13,8 +20,21 @@ const checkUserInput = () => {
         return;
     }
 
-    console.log(numberInput.value);
+    decimalToBinary(parseInt(numberInput.value));
+    numberInput.value = "";
+
 };
+
+/*
+Binary numbers are a base-2 number system. Unlike the base-10 or decimal number system
+we use every day that uses 10 digits (0-9) to form numbers, the binary number system only has two digits, 0 and 1.
+In computer science, these binary digits are called bits, and are the smallest unit of data computers can process. 
+For computers, 0 represents false or "off", and 1 represents true or "on".
+
+In your decimalToBinary function, use the return keyword to return a string of the binary number representation of true.
+
+Note: Binary numbers can be long sequences that start with 0, so they are often represented as strings.
+*/
 
 convertBtn.addEventListener("click", checkUserInput);
 
