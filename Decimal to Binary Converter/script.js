bf27d608ce4ed6d233a645de48777a1b5603336d
me.learn <provinesmilan@gmail.com>
2024-12-10 20:13:05 +0545

    Add event listener for keydown which takes input from keyboard and The key is enter and return keys

--- Decimal to Binary Converter/script.js
+++ Decimal to Binary Converter/script.js
@@ -1,3 +1,12 @@
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
+
+
+convertBtn.addEventListener("click", checkUserInput);
+
+numberInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        checkUserInput();
+    }
+});
