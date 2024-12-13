60037e92ef64c0f9c5a1769e210e693b42d1ce47
me.learn <provinesmilan@gmail.com>
2024-12-13 22:23:28 +0545

    Make a function bubbleSort to learn and sort numbers acc to bubble sort algo

--- Number Sorter/script.js
+++ Number Sorter/script.js
@@ -4,7 +4,7 @@ const sortButton = document.getElementById("sort");
 const sortInputArray = (event) => {
     event.preventDefault();
     const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => Number(dropdown.value));
-
+    updateUI(inputValues);
 }
 
 const updateUI = (array = []) => {
@@ -12,7 +12,12 @@ const updateUI = (array = []) => {
         const outputValueNode = document.getElementById(`output-value-${i}`);
         outputValueNode.innerText = num;
     })
+}
 
+const bubbleSort = (array) => {
+    for (let i = 0; i < array.length; i++) {
+        
+    }
 }
 
 sortButton.addEventListener("click", sortInputArray);
