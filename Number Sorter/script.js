8eaa3c690352c5536d52d86565b08bdfb9272573
me.learn <provinesmilan@gmail.com>
2024-12-13 22:48:58 +0545

    Swap arrays to sort

--- Number Sorter/script.js
+++ Number Sorter/script.js
@@ -4,7 +4,8 @@ const sortButton = document.getElementById("sort");
 const sortInputArray = (event) => {
     event.preventDefault();
     const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => Number(dropdown.value));
-    updateUI(inputValues);
+    const sortedValues = bubbleSort(inputValues);
+    updateUI(sortedValues);
 }
 
 const updateUI = (array = []) => {
@@ -18,6 +19,11 @@ const bubbleSort = (array) => {
     for (let i = 0; i < array.length; i++) {
         for (let j = 0; j < array.length - 1; j++) {
             console.log(array, array[j], array[j + 1]);
+            if (array[j] > array[j + 1]) {
+                const temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
         }
     }
 }
