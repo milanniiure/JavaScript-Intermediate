e29b2a7c29b6d07bab54596a71564da64fc8f2da
me.learn <provinesmilan@gmail.com>
2024-12-13 22:14:44 +0545

    Add a function called updateUI and pass array in forEach method

--- Number Sorter/script.js
+++ Number Sorter/script.js
@@ -3,9 +3,15 @@ const sortButton = document.getElementById("sort");
 
 const sortInputArray = (event) => {
     event.preventDefault();
-    const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => dropdown.value);
-    console.log(inputValues);
+    const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => Number(dropdown.value));
 
+}
+
+const updateUI = (array = []) => {
+    array.forEach((num, i) => {
+        const outputValueNode = document.getElementById(`output-value-${i}`);
+        outputValueNode.innerText = num;
+    })
 
 }
 
