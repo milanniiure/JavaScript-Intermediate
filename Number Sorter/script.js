499cd2e0beb7b979431192b0ba19e39e1ed1f901
me.learn <provinesmilan@gmail.com>
2024-12-14 16:06:45 +0545

    Finish the number sorter project by adding a call back funciton and sort method on inputvalues on sortedValues variable

--- Number Sorter/script.js
+++ Number Sorter/script.js
@@ -4,7 +4,9 @@ const sortButton = document.getElementById("sort");
 const sortInputArray = (event) => {
     event.preventDefault();
     const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => Number(dropdown.value));
-    const sortedValues = bubbleSort(inputValues);
+    const sortedValues = inputValues.sort((a, b) => {
+        return a - b;
+    });
     updateUI(sortedValues);
 }
 
