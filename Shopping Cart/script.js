bcd357a0c948b767f6117547cd60a2c6cbc05523
me.learn <provinesmilan@gmail.com>
2024-12-19 22:55:29 +0545

    Add a shopping cart class and a constructor

--- Shopping Cart/script.js
+++ Shopping Cart/script.js
@@ -101,3 +101,19 @@ products.forEach(
         `;
     }
 );
+
+/*
+The this keyword in JavaScript is used to refer to the current object.
+Depending on where this is used, what it references changes.
+In the case of a class, it refers to the instance of the object being constructed.
+*/
+
+class ShoppingCart {
+    constructor() {
+        this.items = [];
+        this.total = 0;
+        this.taxRate = 8.25;
+    }
+
+    
+};
