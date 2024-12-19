b4149c15d6708e8b8cc1310c8a8d5f0e79b5572f
me.learn <provinesmilan@gmail.com>
2024-12-19 22:48:44 +0545

    Update your products array's foreach method with price, category and id variable

--- Shopping Cart/script.js
+++ Shopping Cart/script.js
@@ -85,11 +85,18 @@ const products = [
     },
 ];
 
+
 products.forEach(
     ({ name, id, price, category }) => {
         dessertCards.innerHTML += `
         <div class="dessert-card">
-            <h2>${name}</h2>  
+            <h2>${name}</h2>
+            <p class="dessert-price">$${price}</p>
+            <p class="product-category">Category: ${category}</p>
+            <button 
+            id="${id}" 
+            class="btn add-to-cart-btn">Add to cart
+            </button>
         </div>
         `;
     }
