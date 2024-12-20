4299f41bf9dd7b747846accef4598daa2e2f0673
me.learn <provinesmilan@gmail.com>
2024-12-20 21:25:18 +0545

    Create another method called calculateTaxes with amount parameter

--- Shopping Cart/script.js
+++ Shopping Cart/script.js
@@ -142,8 +142,14 @@ class ShoppingCart {
         return this.items.length;
     }
 
+    calculateTaxes(amount) {
+        return parseFloat(((this.taxRate / 100) * amount).toFixed(2));
+    }
+
     calculateTotal() {
         const subTotal = this.items.reduce((total, item) => total + item.price, 0);
+        const tax = this.calculateTaxes(subTotal);
+        this.total = subTotal + tax;
     }
 };
 
