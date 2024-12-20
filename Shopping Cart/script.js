48d34d15b8393699e814fb35595298832161ccfc
me.learn <provinesmilan@gmail.com>
2024-12-20 21:35:25 +0545

    Update calculate total method with textContent updates

--- Shopping Cart/script.js
+++ Shopping Cart/script.js
@@ -150,6 +150,10 @@ class ShoppingCart {
         const subTotal = this.items.reduce((total, item) => total + item.price, 0);
         const tax = this.calculateTaxes(subTotal);
         this.total = subTotal + tax;
+        cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
+        cartTaxes.textContent = `$${tax.toFixed(2)}`;
+        cartTotal.textContent = `$${this.total.toFixed(2)}`;
+        return this.total;
     }
 };
 
