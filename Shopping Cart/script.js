f81d4122a958a3d44c742da17f4033916d585e07
me.learn <provinesmilan@gmail.com>
2024-12-20 21:50:40 +0545

    Add method to clear cart and clear cart only after user confirms

--- Shopping Cart/script.js
+++ Shopping Cart/script.js
@@ -142,6 +142,27 @@ class ShoppingCart {
         return this.items.length;
     }
 
+    clearCart() {
+        if (!this.items.length) {
+            alert("Your shopping cart is already empty");
+            return;
+        }
+
+        const isCartCleared = confirm(
+            "Are you sure you want to clear all items from your shopping cart?"
+        );
+
+        if (isCartCleared) {
+            this.items = [];
+            this.total = 0;
+            productsContainer.innerHTML = "";
+            totalNumberOfItems.textContent = 0;
+            cartSubTotal.textContent = 0;
+            cartTaxes.textContent = 0;
+            cartTotal.textContent = 0;
+        }
+    }
+
     calculateTaxes(amount) {
         return parseFloat(((this.taxRate / 100) * amount).toFixed(2));
     }
@@ -165,6 +186,7 @@ const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
         btn.addEventListener("click", (event) => {
             cart.addItem(Number(event.target.id), products);
             totalNumberOfItems.textContent = cart.getCounts();
+            cart.calculateTotal();
         })
     }
 );
