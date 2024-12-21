eb9a8616aea379f68034bdc507cec3793c00b608
me.learn <provinesmilan@gmail.com>
2024-12-21 20:38:32 +0545

    create draw method and update method where player and object interaction logic will be setup

--- Platformer Game/script.js
+++ Platformer Game/script.js
@@ -28,4 +28,31 @@ class Player {
         this.width = proportionalSize(40);
         this.height = proportionalSize(40);
     }
+    draw() {
+        ctx.fillStyle = "#99c9ff";
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+    update() {
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+
+        if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+            if (this.position.y < 0) {
+                this.position.y = 0;
+                this.velocity.y = gravity;
+            }
+            this.velocity.y += gravity;
+        } else {
+            this.velocity.y = 0;
+        }
+
+        if (this.position.x < this.width) {
+            this.position.x = this.width;
+        }
+
+        if (this.position.x >= canvas.width - this.width * 2) {
+            this.position.x = canvas.width - this.width * 2;
+        }
+    }
 }
