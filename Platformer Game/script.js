f8e3d36d15790dfce1fa3f135093984457689d12
me.learn <provinesmilan@gmail.com>
2024-12-21 20:08:22 +0545

    Add a class called player and a constructor method and set position, velocity, width , height

--- Platformer Game/script.js
+++ Platformer Game/script.js
@@ -13,3 +13,19 @@ let isCheckpointCollisionDetectionActive = true;
 const proportionalSize = (size) => {
     return innerHeight < 500 ? Math.ceil((size / 500) * innerHeight) : size;
 }
+
+
+class Player {
+    constructor() {
+        this.position = {
+            x: proportionalSize(10),
+            y: proportionalSize(400),
+        };
+        this.velocity = {
+            x: 0,
+            y: 0,
+        };
+        this.width = proportionalSize(40);
+        this.height = proportionalSize(40);
+    }
+}
