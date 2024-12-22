5a1f1645bad1da129492cb449256b5b15f1311ec
me.learn <provinesmilan@gmail.com>
2024-12-22 19:24:20 +0545

    Make a new object called player and a arrow back function to animate , clear and render next frames using requestAnimationFrame and clearRect webAPI

--- Platformer Game/script.js
+++ Platformer Game/script.js
@@ -56,3 +56,12 @@ class Player {
         }
     }
 }
+
+
+const player = new Player();
+
+const animate = () => {
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    player.update();
+}
