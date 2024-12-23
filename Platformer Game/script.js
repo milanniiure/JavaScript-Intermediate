effc8b26b696d3e9d12cbbc00880bc94adf50dd4
me.learn <provinesmilan@gmail.com>
2024-12-23 18:34:38 +0545

    Add if else statement to make the moving platforms

--- Platformer Game/script.js
+++ Platformer Game/script.js
@@ -90,6 +90,10 @@ const platformPositions = [
     { x: 4700, y: proportionalSize(150) },
 ];
 
+const platforms = platformPositions.map(
+    (platform) => new Platform(platform.x, platform.y)
+);
+
 const animate = () => {
     requestAnimationFrame(animate);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -101,7 +105,18 @@ const animate = () => {
         player.velocity.x = -5;
     } else {
         player.velocity.x = 0;
+
+        if (keys.rightKey.pressed && isCheckpointCollisionDetectionActive) {
+            platforms.forEach((platform) => {
+                platform.position.x -= 5;
+            });
+        } else if (keys.leftKey.pressed && isCheckpointCollisionDetectionActive) {
+            platforms.forEach((platform) => {
+                platform.position.x += 5;
+            });
+        }
     }
+    
 }
 
 
