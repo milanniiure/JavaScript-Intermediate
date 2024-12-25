7b051d0f15b6f77971c248d9106d6d5c05e1136c
me.learn <provinesmilan@gmail.com>
2024-12-25 22:03:11 +0545

    start declaring variables, do getElementById and do querySelector

---
+++ Dice Game/script.js
@@ -0,0 +1,18 @@
+const listOfAllDice = document.querySelectorAll(".die");
+const scoreInputs = document.querySelectorAll('#score-options input');
+const scoreSpans = document.querySelectorAll('#score-options span');
+const roundElement = document.getElementById("current-round");
+const rollsElement = document.getElementById("current-round-rolls");
+const totalScoreElement = document.getElementById("total-score");
+const scoreHistory = document.getElementById("score-history");
+const rollDiceBtn = document.getElementById("roll-dice-btn");
+const keepScoreBtn = document.getElementById("keep-score-btn");
+const rulesBtn = document.getElementById("rules-btn");
+const rulesContainer = document.querySelector(".rules-container");
+
+
+let isModalShowing = false;
+let diceValuesArr = [];
+let rolls = 0;
+let score = 0;
+let round = 1;
