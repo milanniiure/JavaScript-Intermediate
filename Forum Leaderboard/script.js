4695a33396a1e3f9480b708876183bc235f857bc
me.learn <provinesmilan@gmail.com>
2024-12-31 21:16:27 +0545

    Update forumCategory func which returns urls with category

--- Forum Leaderboard/script.js
+++ Forum Leaderboard/script.js
@@ -29,6 +29,13 @@ const forumCategory = (id) => {
         selectedCategory.category = "General";
         selectedCategory.id = 1;
     }
+    const url = `${forumCategoryUrl}${selectedCategory.className}/${id}`;
+    const linkText = selectedCategory.category;
+    const linkClass = `category ${selectedCategory.className}`;
+
+    return `<a href="${url}" class="${linkClass}" target="_blank">
+    ${linkText}
+    </a>`;
 
 };
 
@@ -93,6 +100,7 @@ const showLatestPosts = (data) => {
         <tr>
             <td>
             <p class="post-title">${title}</p>
+            ${forumCategory(category_id)}
             </td>
             <td></td>
             <td>${posts_count - 1}</td>
