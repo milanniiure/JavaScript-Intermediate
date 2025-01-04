61fa2b28eadc102d8e431d8360ef81bb4e97a3e5
me.learn <84887409+milanniiure@users.noreply.github.com>
2025-01-04 21:47:28 +0545

    Add a function to check user Input which alerts user to input a value

--- Palindrome Checker/script.js
+++ Palindrome Checker/script.js
@@ -1,5 +1,9 @@
 const checkBtn = document.getElementById("check-btn");
 
-
+const userInput = () => {
+    if (userInput === "") {
+        alert("Please input a value");
+    }
+}
 
 checkBtn.addEventListener("click", userInput());
