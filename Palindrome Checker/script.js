3d91a0b057a6be13ce94bbe4ffcb371d1e9b7dbb
me.learn <provinesmilan@gmail.com>
2025-01-07 19:42:28 +0545

    Changed the typo

--- Palindrome Checker/script.js
+++ Palindrome Checker/script.js
@@ -1,4 +1,4 @@
-const textInput = document.getElementById("textn-input");
+const textInput = document.getElementById("text-input");
 const checkBtn = document.getElementById("check-btn");
 const resultDiv = document.getElementById("result");
 
