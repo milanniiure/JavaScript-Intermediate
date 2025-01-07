1dce43b7fd3649d9f6891147a4c60524e14b2cf0
me.learn <84887409+milanniiure@users.noreply.github.com>
2025-01-07 19:22:01 +0545

    Adding another div for output results

--- Palindrome Checker/script.js
+++ Palindrome Checker/script.js
@@ -1,11 +1,18 @@
+const textInput = document.getElementById("textn-input");
 const checkBtn = document.getElementById("check-btn");
 
-const userInput = () => {
+const checkPalindrome = () => {
+    const input = textInput.value;
+
     if (input === "") {
         alert("Please input a value");
+        return;
     }
-    const input = replace(/[^a-zA-Z0-9]/g, '').toLowerCase(userInput);
-    
+    const normalizedInput = input.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const reversedInput = normalizedInput.split('').reverse().join('');
+    const isPalindrome = normalizedInput === reversedInput;
+    const resultMessage = isPalindrome ? "The input is a palindrome." : "The input is not a palindrome.";
+    alert(resultMessage);    
 }
 
 checkBtn.addEventListener("click", userInput);
