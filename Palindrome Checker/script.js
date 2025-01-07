41f61e82ae126ce3f2e782e20aa83af9a319b956
me.learn <84887409+milanniiure@users.noreply.github.com>
2025-01-07 19:24:18 +0545

    Corrected Version

--- Palindrome Checker/script.js
+++ Palindrome Checker/script.js
@@ -1,18 +1,19 @@
 const textInput = document.getElementById("textn-input");
 const checkBtn = document.getElementById("check-btn");
+const resultDiv = document.getElementById("result");
 
 const checkPalindrome = () => {
-    const input = textInput.value;
+    const input = textInput.value.trim();
 
     if (input === "") {
-        alert("Please input a value");
+        resultDiv.textContent = "Please input a value";
         return;
     }
     const normalizedInput = input.toLowerCase().replace(/[^a-z0-9]/g, '');
     const reversedInput = normalizedInput.split('').reverse().join('');
     const isPalindrome = normalizedInput === reversedInput;
     const resultMessage = isPalindrome ? "The input is a palindrome." : "The input is not a palindrome.";
-    alert(resultMessage);    
+    resultDiv.textContent = resultMessage;  
 }
 
-checkBtn.addEventListener("click", userInput);
+checkBtn.addEventListener("click", checkPalindrome);
