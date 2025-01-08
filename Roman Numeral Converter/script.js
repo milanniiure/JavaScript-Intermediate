187344cab9dc50e0c8bfdccb72c0c5440a456da9
me.learn <provinesmilan@gmail.com>
2025-01-08 22:39:52 +0545

    Add values and corresponding numerals inside an array

--- Roman Numeral Converter/script.js
+++ Roman Numeral Converter/script.js
@@ -4,10 +4,25 @@ const convertBtn = document.getElementById("convert-btn");
 
 const resultDiv = document.getElementById("output");
 
+const romanNumerals = [
+    { value: 1000, numeral: "M" },
+    { value: 900, numeral: "CM" },
+    { value: 500, numeral: "D" },
+    { value: 400, numeral: "CD" },
+    { value: 100, numeral: "C" },
+    { value: 90, numeral: "XC" },
+    { value: 50, numeral: "L" },
+    { value: 40, numeral: "XL" },
+    { value: 10, numeral: "X" },
+    { value: 9, numeral: "IX" },
+    { value: 5, numeral: "V" },
+    { value: 4, numeral: "IV" },
+    { value: 1, numeral: "I" }
+];
 
 const convertedRoman = () => {
 
-    const input = numberInput.value.trim();
+    const input = isNaN(numberInput.value.trim());
     if (input === "") {
         resultDiv.textContent = "Please enter a valid number";
         return;
@@ -19,6 +34,10 @@ const convertedRoman = () => {
     } else {
 
     }
-
 };
+
+function  intoRoman (){
+
+}
+
 convertBtn.addEventListener("click", convertedRoman);
