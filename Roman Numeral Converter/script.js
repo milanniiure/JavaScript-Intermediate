0a543ff6eb033c9045389d7368da3ee242520e07
me.learn <provinesmilan@gmail.com>
2025-01-08 21:52:05 +0545

    Make logical conditions which numbers to enter

--- Roman Numeral Converter/script.js
+++ Roman Numeral Converter/script.js
@@ -6,11 +6,18 @@ const resultDiv = document.getElementById("output");
 
 
 const convertedRoman = () => {
-    
+
     const input = numberInput.value.trim();
     if (input === "") {
         resultDiv.textContent = "Please enter a valid number";
         return;
+    } else if (input <= 0) {
+        resultDiv.textContent = "Please enter a number greater than or equal to 1";
+    } else if (input >= 4000) {
+        resultDiv.textContent = "Please enter a number less than or equal to 3999";
+        return;
+    } else {
+
     }
 
 };
