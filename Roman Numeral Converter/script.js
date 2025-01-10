e685051c434485c65668e0a0b7dec3034b42ff28
me.learn <provinesmilan@gmail.com>
2025-01-10 23:19:28 +0545

    Draft 2 another logic

--- Roman Numeral Converter/script.js
+++ Roman Numeral Converter/script.js
@@ -47,6 +47,29 @@ convertBtn.addEventListener("click", convertedRoman);
 /*
 LOGIC TO CONVERT INTEGER TO ROMAN NUMERAL
 
+TABLE OF ROMAN NUMERALS:
+1000 = M
+900 = CM
+500 = D
+400 = CD
+100 = C
+90 = XC
+50 = L
+40 = XL
+10 = X
+9 = IX
+5 = V
+4 = IV
+1 = I
+
+Now, let's say we have a number 3549
+1. Divide 3549 by 1000, we get 3. So, 3 * 1000 = 3000. Add 3000 to roman. roman = "MMM"
+2. Subtract 3000 from 3549, we get 549
+3. Divide 549 by 500, we get 1. So, 1 * 500 = 500. Add 500 to roman. roman = "MMMD"
+4. Subtract 500 from 549, we get 49
+5. Divide 49 by 40, we get 1. So, 1 * 40 = 40. Add 40 to roman. roman = "MMMDXL"
+6. Subtract 40 from 49, we get 9
+7. Divide 9 by 9, we get 1. So, 1 * 9 = 9. Add 9 to roman. roman = "MMMDXLIX"
 
 
 Finally, roman = "MMMDXLIX"
