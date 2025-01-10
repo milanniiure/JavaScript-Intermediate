7d3d6de87423490729270fde11b051b8a1845144
me.learn <provinesmilan@gmail.com>
2025-01-10 23:37:48 +0545

    Add roman numeral table into an array making it array of array

--- Roman Numeral Converter/script.js
+++ Roman Numeral Converter/script.js
@@ -4,21 +4,34 @@ const convertBtn = document.getElementById("convert-btn");
 
 const resultDiv = document.getElementById("output");
 
-const romanTable = [
-    ['M', 1000],
-    ['CM', 900],
-    ['D', 500],
-    ['CD', 400],
-    ['C', 100],
-    ['XC', 90],
-    ['L', 50],
-    ['XL', 40],
-    ['X', 10],
-    ['IX', 9],
-    ['V', 5],
-    ['IV', 4],
-    ['I', 1]
-];
+function convertToRoman() {
+    const romanNumerals = [
+        ['M', 1000],
+        ['CM', 900],
+        ['D', 500],
+        ['CD', 400],
+        ['C', 100],
+        ['XC', 90],
+        ['L', 50],
+        ['XL', 40],
+        ['X', 10],
+        ['IX', 9],
+        ['V', 5],
+        ['IV', 4],
+        ['I', 1]
+    ];
+
+    let roman = "";
+    for (const [symbol, value] of romanNumerals) {
+        while (num >= value) {
+            roman += symbol;
+            num -= value;
+        }
+    }
+
+    return roman;
+}
+
 
 
 const convertedRoman = () => {
@@ -37,7 +50,7 @@ const convertedRoman = () => {
     }
 };
 
-function  intoRoman (){
+function intoRoman() {
     let roman = "";
 }
 
