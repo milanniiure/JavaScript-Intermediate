04668bb6ea88eb60074a18eb88e5f94d78f23c04
me.learn <provinesmilan@gmail.com>
2025-01-11 22:53:52 +0545

    Going for trial and error method

--- Telephone Number Validator/script.js
+++ Telephone Number Validator/script.js
@@ -1,5 +1,19 @@
-const checkBtn = document.getElementById("user-input");
+const userInput = document.getElementById("user-input");
+
+const checkBtn = document.getElementById("check-btn");
 
 const clearBtn = document.getElementById("clear-btn");
 
 const resultDiv = document.getElementById("results-div");
+
+const phoneRegex = /^(\+?234|0)([789]\d{9})$/;
+
+checkBtn.addEventListener("click", () => {
+    const userInput = document.getElementById("phone-number").value;
+
+    if (phoneRegex.test(userInput)) {
+        resultDiv.innerHTML = `<p class="success">Valid Phone Number</p>`;
+    } else {
+        resultDiv.innerHTML = `<p class="error">Invalid Phone Number</p>`;
+    }
+});
