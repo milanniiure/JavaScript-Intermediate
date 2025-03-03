191f2a5d6e1a7adf43aec8901a25bb625ae22943
me.learn <provinesmilan@gmail.com>
2025-03-03 13:17:45 +0545

    testing us phone number validator

--- Telephone Number Validator/script.js
+++ Telephone Number Validator/script.js
@@ -6,17 +6,34 @@ const clearBtn = document.getElementById("clear-btn");
 
 const resultDiv = document.getElementById("results-div");
 
-//this will check if the input field is empty
-function validatePhoneNumber(event){
+//checks if the phone number is valid
+function validatePhoneNumber(event) {
     event.preventDefault();
+
+    //check if the input field is empty after trimming unnecessary spaces
     let phoneNumber = userInput.value.trim();
-    
-    if(phoneNumber === ""){
+    if (phoneNumber === "") {
         alert("Please provide a phone number");
+        return;
+    }
+
+    //regex for US phone number
+    const phoneRegex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+    // Test the trimmed user input against the phone number regex
+    let phoneTest = phoneRegex.test(phoneNumber);//true if valid false it not
+    if (phoneTest) {
+        let msg = `Valid US number: ${phoneNumber}`;
+        console.log(msg);
+        resultDiv.textContent = msg;
+
+    } else {
+        let msg = `Invalid US number: ${phoneNumber}`;
+        console.log(msg);
+        resultDiv.textContent = msg;
     }
 }
 //clears the input and result div
-clearBtn.addEventListener("click", function(){
+clearBtn.addEventListener("click", function () {
     userInput.value = "";
     resultDiv.textContent = "";
 });
