86fd50f6f858264eeb97864eec020a7228d4aaf9
me.learn <provinesmilan@gmail.com>
2025-03-03 12:28:09 +0545

    Added an event listener which clears the input and result div

--- Telephone Number Validator/script.js
+++ Telephone Number Validator/script.js
@@ -6,14 +6,19 @@ const clearBtn = document.getElementById("clear-btn");
 
 const resultDiv = document.getElementById("results-div");
 
+//this will check if the input field is empty
 function validatePhoneNumber(event){
     event.preventDefault();
     let phoneNumber = userInput.value.trim();
     
     if(phoneNumber === ""){
-        alert("Please enter a phone number");
+        alert("Please provide a phone number");
     }
-    
 }
-
+//clears the input and result div
+clearBtn.addEventListener("click", function(){
+    userInput.value = "";
+    resultDiv.textContent = "";
+});
+//event listener for the check button
 checkBtn.addEventListener("click", validatePhoneNumber);   
