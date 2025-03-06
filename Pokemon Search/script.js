9b667a63c3b5af7f7c6b227793a5c578bec5f6ef
me.learn <provinesmilan@gmail.com>
2025-03-06 16:42:01 +0545

    Variables declared to get element input using ids

---
+++ Pokemon Search/script.js
@@ -0,0 +1,19 @@
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+
+const pokemonName = document.getElementById('pokemonName');
+const pokemonId = document.getElementById('pokemonId');
+const weight = document.getElementById('weight');
+const height = document.getElementById('height');
+const types = document.getElementById('types');
+
+const hp = document.getElementById('hp');
+const attack = document.getElementById('attack');
+const defense = document.getElementById('defense');
+const specialAttack = document.getElementById('specialAttack');
+const specialDefense = document.getElementById('specialDefense');
+const speed = document.getElementById('speed');
+
+searchButton.addEventListener("click", ()=>{
+    const pokemon = searchInput.value.toLowerCase();
+})
